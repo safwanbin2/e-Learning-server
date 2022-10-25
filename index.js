4266ba7f13747b99040d6e3d9dc4341e9cb207c0
Safwan <safwanridwan321@gmail.com>
2022-10-25 11:29:02 +0600

    done with description

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('success');
+    res.send('server running fine');
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
 
 
