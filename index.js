bec21cac071b25a0cd72614b5492fc3b6a2f9a99
Safwan <safwanridwan321@gmail.com>
2022-10-25 11:32:45 +0600

    almost done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const courses = require('./data/courses.json');
+const coursesDetails = require('./data/coursesDetails.json');
 
 app.use(cors());
 
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
     res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selected = coursesDetails.find(c => c.id === id);
+    res.send(selected);
 })
 
 
